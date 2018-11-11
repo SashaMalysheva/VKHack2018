0c9d7b61460a2512c773fbd6b55e202b73245df2
Sasha Malysheva <malyshevasasha777@gmail.com>
2018-11-11 03:20:32 +0300

    find_nearest_img .csv -> pickle

--- find_nearest_img.py
+++ find_nearest_img.py
@@ -4,20 +4,25 @@ import numpy as np
 import sys
 import getopt
 from PIL import Image
+import pickle
+import argparse
+import logging
 
 root_path = os.path.dirname(sys.modules['__main__'].__file__)
 
-def choose_nearest(inputfile, outputfile):
-    paintings = np.genfromtxt('painting.csv', delimiter=',')
+def choose_nearest(dataset_name, inputfile, outputfile):
+    with open(dataset_name + '.p', 'rb') as fp:
+        paintings = pickle.load(fp)
+
     filepath = os.path.join(root_path, inputfile)
     image = face_recognition.load_image_file(filepath)
     photo = face_recognition.face_encodings(image)[0]
 
-    nearest_img_index = np.argmin(np.array([np.sum(np.sqrt((photo - vector[0])**2)) for vector in paintings]))
+    key = min(paintings.items(), key=lambda x: np.sum(np.sqrt((photo - np.array(x[1]))**2)))[0]
 
-    folder = '' if paintings[nearest_img_index, -1] == 1 else ' 2'
-    name = str(int(paintings[nearest_img_index, -2])).zfill(4)
-    directory = os.path.join(root_path, 'dataset_updated' + folder, 'training_set', 'painting', name + '.jpg')
+    folder = '' if key[0] == '1' else ' 2'
+    name = key[1:]
+    directory = os.path.join(root_path, 'dataset_updated' + folder, 'training_set', dataset_name, name + '.jpg')
     image = Image.open(directory)
     if outputfile == '':
         image.show()
@@ -25,26 +30,36 @@ def choose_nearest(inputfile, outputfile):
         image.save(os.path.join(root_path, outputfile))
 
 
-def main(argv):
-    inputfile = ''
-    outputfile = ''
-    ans_photo =''
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:")
-    except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i"):
-            inputfile = arg
+def _parse_config():
 
-        elif opt in ("-o") and inputfile != '':
-            outputfile = arg
 
-    choose_nearest(inputfile, outputfile)
+    parser = argparse.ArgumentParser(
+        description='Nearest img for given image',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("--d",
+                        help="name_of_dataset 'sculpture', 'painting', 'iconography', 'engraving', 'drawings'")
+    parser.add_argument("--i",
+                        help="inputfile")
+    parser.add_argument("--o",
+                        help="outputfile")
+    config = parser.parse_args()
+
+    logging.basicConfig(
+        format='%(asctime)s | %(message)s',
+        handlers=[
+            logging.StreamHandler()
+        ],
+        level=logging.INFO
+    )
+
+    return config
+
+def main(config):
+    inputfile = config.i
+    outputfile = config.o
+    dataset_name = config.d
+    choose_nearest(dataset_name, inputfile, outputfile)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(_parse_config())
